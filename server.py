7e9de1b612e4b6b7e310465dcd5a213688158458
Rupesh Mangal <rupesh.code@gmail.com>
2016-12-23 15:54:39 +0530

    updated

--- server.py
+++ server.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route('/')
 def index():
 	return render_template("main.html")
